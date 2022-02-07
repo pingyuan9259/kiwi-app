623d5414cafb97bc617a603468263a953f4740a1
pingyuan9259 <pingyuan9259@gmail.com>
2022-02-07 21:49:38 +0800

    "update"

--- publish.sh
+++ publish.sh
@@ -13,9 +13,9 @@ git pull
 git add -A
 git commit -m \"$commitText\"
 
-# 发布Npm
+# 发布Npm，发布带有@命名空间的包默认为私有包（收费），需要添加--access public参数进行公有化
 npm version patch
-npm publish
+npm publish --access public
 
 # 将代码推送至仓库
 git push
